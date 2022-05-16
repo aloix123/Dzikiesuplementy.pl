0336db5ac80d6f19cf82aeee87764319d94f8047
Fragewskyy <75323319+Fragewskyy@users.noreply.github.com>
2022-05-16 16:53:41 +0200

    Update StuffApplication.java

--- src/main/java/project/stuff/StuffApplication.java
+++ src/main/java/project/stuff/StuffApplication.java
@@ -1,14 +1,14 @@
-package project.stuff;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication()
-public class StuffApplication {
+@SpringBootApplication(scanBasePackages = {"com.example.demo.service"})
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StuffApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
