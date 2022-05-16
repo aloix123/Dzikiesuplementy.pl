c9d12841d1790c1f49e3de8be49b304deb6e9498
Fragewskyy <75323319+Fragewskyy@users.noreply.github.com>
2022-05-16 16:40:54 +0200

    Update ClientController.java

--- src/main/java/project/stuff/controller/ClientController.java
+++ src/main/java/project/stuff/controller/ClientController.java
@@ -1,21 +1,53 @@
-//package project.stuff.controller;
-//
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import project.stuff.repository.ClientRepository;
-//
-//@RestController
-//@RequestMapping("/api/clients")
-//public class ClientController {
-//    @GetMapping("/{login}{password}")
-//    public ResponseEntity<Boolean> validateLogin(@PathVariable String login, @PathVariable String password){
-////        try {
-////            ClientRepository clientRepository;
-////            clientRepository.getByL
-////        }
-////    }
-//
-//}
+package com.example.demo.controller;
+
+import com.example.demo.model.Client;
+import com.example.demo.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model){
+        model.addAttribute("registerRequest", new Client());
+        return "register_page";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage(Model model){
+        model.addAttribute("loginRequest", new Client());
+        return "login_page";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute Client client){
+        System.out.println("register request: " + client);
+        Client reqisteredClient = clientService.registerClient(client.getName(), client.getSurname(), client.getEmail(),
+                client.getPassword());
+        return reqisteredClient == null ? "error_page" : "redirect:/login";
+    }
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute Client client, Model model){
+        System.out.println("login request: " + client);
+        Client authenticated = clientService.authenticate(client.getEmail(),
+                client.getPassword());
+        if(authenticated != null) {
+            model.addAttribute("userEmail", authenticated.getEmail());
+            return "personal_page";
+        }else {
+            return "error_page";
+        }
+    }
+}
