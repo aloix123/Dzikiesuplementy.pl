4c06377801fc8232ad848c6f40bb339f16e622e5
Fragewskyy <75323319+Fragewskyy@users.noreply.github.com>
2022-05-16 16:50:37 +0200

    Update ClientRepository.java

--- src/main/java/project/stuff/repository/ClientRepository.java
+++ src/main/java/project/stuff/repository/ClientRepository.java
@@ -1,10 +1,15 @@
-package project.stuff.repository;
+package com.example.demo.repository;
 
 
+import com.example.demo.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
-import project.stuff.model.Client;
+
+
+import java.util.Optional;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    Optional<Client> findByEmailAndPassword(String email, String password);
 }
