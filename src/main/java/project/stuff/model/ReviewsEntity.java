fe0dc788f65bb9d5c0d2797a237153e4cbca3452
Fragewskyy <75323319+Fragewskyy@users.noreply.github.com>
2022-05-16 16:49:57 +0200

    Update ReviewsEntity.java

--- src/main/java/project/stuff/model/ReviewsEntity.java
+++ src/main/java/project/stuff/model/ReviewsEntity.java
@@ -1,56 +1,44 @@
-package project.stuff.model;
+package com.example.demo.model;
 
 import javax.persistence.*;
-import java.util.Objects;
 
 @Entity
-@Table(name = "reviews", schema = "sys", catalog = "")
-public class ReviewsEntity {
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+@Table(name = "reviews")
+public class Review {
     @Id
-    @Column(name = "review_id")
-    private int reviewId;
-    @Basic
-    @Column(name = "product_id")
-    private int productId;
-    @Basic
-    @Column(name = "stars_amount")
-    private int starsAmount;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id", nullable = false)
+    private Integer id;
 
-    public int getReviewId() {
-        return reviewId;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "stars_amount", nullable = false)
+    private Integer starsAmount;
+
+    public Integer getId() {
+        return id;
     }
 
-    public void setReviewId(int reviewId) {
-        this.reviewId = reviewId;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
-    public int getProductId() {
-        return productId;
+    public Product getProduct() {
+        return product;
     }
 
-    public void setProductId(int productId) {
-        this.productId = productId;
+    public void setProduct(Product product) {
+        this.product = product;
     }
 
-    public int getStarsAmount() {
+    public Integer getStarsAmount() {
         return starsAmount;
     }
 
-    public void setStarsAmount(int starsAmount) {
+    public void setStarsAmount(Integer starsAmount) {
         this.starsAmount = starsAmount;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ReviewsEntity that = (ReviewsEntity) o;
-        return reviewId == that.reviewId && productId == that.productId && starsAmount == that.starsAmount;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(reviewId, productId, starsAmount);
-    }
 }
