6cf52fd870345998295fc93b5886a9842310cd18
Fragewskyy <75323319+Fragewskyy@users.noreply.github.com>
2022-05-16 16:44:27 +0200

    Update Client.java

--- src/main/java/project/stuff/model/Client.java
+++ src/main/java/project/stuff/model/Client.java
@@ -1,101 +1,49 @@
-package project.stuff.model;
+package com.example.demo.model;
 
 import javax.persistence.*;
-import java.sql.Date;
-import java.util.Objects;
+import java.time.LocalDate;
 
 @Entity
-@Table(name = "clients", schema = "sys", catalog = "")
+@Table(name = "clients")
 public class Client {
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
-    @Column(name = "client_id")
-    private int clientId;
-    @Basic
-    @Column(name = "name")
-    private String name;
-    @Basic
-    @Column(name = "order_id")
-    private int orderId;
-    @Basic
-    @Column(name = "surname")
-    private String surname;
-    @Basic
-    @Column(name = "email")
-    private String email;
-    @Basic
-    @Column(name = "password")
-    private String password;
-    @Basic
-    @Column(name = "birth_date")
-    private Date birthDate;
-    @Basic
-    @Column(name = "address")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "client_id", nullable = false)
+    private Integer id;
+
+    @Column(name = "address", length = 45)
     private String address;
-    @Basic
-    @Column(name = "postal_code")
-    private String postalCode;
-    @Basic
-    @Column(name = "city")
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "city", length = 45)
     private String city;
-    @Basic
-    @Column(name = "phone_number")
+
+    @Column(name = "email", length = 45)
+    private String email;
+
+    @Column(name = "name", length = 45)
+    private String name;
+
+    @Column(name = "password", length = 45)
+    private String password;
+
+    @Column(name = "phone_number", length = 45)
     private String phoneNumber;
 
-    public int getClientId() {
-        return clientId;
+    @Column(name = "postal_code", length = 45)
+    private String postalCode;
+
+    @Column(name = "surname", length = 45)
+    private String surname;
+
+    public Integer getId() {
+        return id;
     }
 
-    public void setClientId(int clientId) {
-        this.clientId = clientId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(int orderId) {
-        this.orderId = orderId;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Date getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(Date birthDate) {
-        this.birthDate = birthDate;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
     public String getAddress() {
@@ -106,12 +54,12 @@ public class Client {
         this.address = address;
     }
 
-    public String getPostalCode() {
-        return postalCode;
+    public LocalDate getBirthDate() {
+        return birthDate;
     }
 
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
     }
 
     public String getCity() {
@@ -122,6 +70,30 @@ public class Client {
         this.city = city;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     public String getPhoneNumber() {
         return phoneNumber;
     }
@@ -130,16 +102,20 @@ public class Client {
         this.phoneNumber = phoneNumber;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Client that = (Client) o;
-        return clientId == that.clientId && orderId == that.orderId && Objects.equals(name, that.name) && Objects.equals(surname, that.surname) && Objects.equals(email, that.email) && Objects.equals(password, that.password) && Objects.equals(birthDate, that.birthDate) && Objects.equals(address, that.address) && Objects.equals(postalCode, that.postalCode) && Objects.equals(city, that.city) && Objects.equals(phoneNumber, that.phoneNumber);
+    public String getPostalCode() {
+        return postalCode;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(clientId, name, orderId, surname, email, password, birthDate, address, postalCode, city, phoneNumber);
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
     }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
 }
