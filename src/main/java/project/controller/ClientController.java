8d131bbf83d8e2f180c0dc6a86633ca01112c6dd
Fragewskyy <kontakt.fragewskyy@gmail.com>
2022-05-31 15:55:52 +0200

    👉🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👇🏿👈🏿 👉🏿👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👇🏾👈🏿 👉🏿👉🏾👇🏽👇🏽👇🏽👇🏽👇🏽👇🏽👇🏽👈🏾👈🏿 👉🏿👉🏾👉🏽👇🏼👇🏼👇🏼👇🏼👇🏼👈🏽👈🏾👈🏿 👉🏿👉🏾👉🏽👉🏼👇🏻👇🏻👇🏻👈🏼👈🏽👈🏾👈🏿 👉🏿👉🏾👉🏽👉🏼👉🏻 ඞ-impostor paweł to impostor 👈🏻👈🏼👈🏽👈🏾👈🏿 👉🏿👉🏾👉🏽👉🏼👆🏻👆🏻👆🏻👈🏼👈🏽👈🏾👈🏿 👉🏿👉🏾👉🏽👆🏼👆🏼👆🏼👆🏼👆🏼👈🏽👈🏾👈🏿 👉🏿👉🏾👆🏽👆🏽👆🏽👆🏽👆🏽👆🏽👆🏽👈🏾👈🏿 👉🏿👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👆🏾👈🏿 👉🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👆🏿👈🏿
    jak sie nazywa zbyt podpieczony tost? odp:spalony kekw

--- src/main/java/project/controller/ClientController.java
+++ src/main/java/project/controller/ClientController.java
@@ -1,6 +1,8 @@
 package project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,53 +20,58 @@ public class ClientController {
 
     @GetMapping("/")
     public String viewHomePage() {
-        return "index.html";
+        return "redirect:/index.html";
+    }
+
+    @GetMapping("/error")
+    public String viewErrorPage() {
+        return "error_page.html";
     }
 
     @GetMapping("/register")
-    public String getRegisterPage(Model model){
+    public String getRegisterPage(Model model) {
         model.addAttribute("registerRequest", new Client());
         return "register_page";
     }
 
     @GetMapping("/login")
-    public String getLoginPage(Model model){
+    public String getLoginPage(Model model) {
         model.addAttribute("loginRequest", new Client());
         return "login_page";
     }
 
     @GetMapping("/personal")
-    public String personalPage(){
+    public String personalPage() {
         return "personal_page";
     }
 
     @GetMapping("/creatine")
-    public String creatinePage(){
+    public String creatinePage() {
         return "Creatine";
     }
 
     @GetMapping("/preworkout")
-    public String preworkoutPage(){
+    public String preworkoutPage() {
         return "Preworkout";
     }
 
     @GetMapping("/whey")
-    public String wheyPage(){
+    public String wheyPage() {
         return "Whey";
     }
 
     @GetMapping("/newspaper")
-    public String newspaperPage(){
+    public String newspaperPage() {
         return "Newspaper";
     }
 
     @GetMapping("/cart")
-    public String cartPage(){
+    public String cartPage() {
         return "koszyk";
     }
 
     @PostMapping("/register")
-    public String register(@ModelAttribute Client client){
+    public String register(@ModelAttribute Client client) {
         System.out.println("register request: " + client);
         Client reqisteredClient = clientService.registerClient(client.getName(), client.getSurname(), client.getEmail(),
                 client.getPassword());
@@ -72,16 +79,18 @@ public class ClientController {
     }
 
     @PostMapping("/login")
-    public String login(@ModelAttribute Client client, Model model){
+    public ResponseEntity<Client> login(@ModelAttribute Client client, Model model) {
         System.out.println("login request: " + client);
         Client authenticated = clientService.authenticate(client.getEmail(),
                 client.getPassword());
-        if(authenticated != null) {
+        if (authenticated != null) {
             model.addAttribute("userEmail", authenticated.getEmail());
             isLoggedIn = true;
-            return "index";
-        }else {
-            return "error_page";
+            System.out.println(client.toString());
+            return new ResponseEntity<>(client, HttpStatus.OK);
+        } else {
+            viewErrorPage();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 }
